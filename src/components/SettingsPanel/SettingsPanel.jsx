a26c4b8aec9e89ff9b9a81f9e051a3e2d96b86aa
Zachary Ringwood <zringwood@gmail.com>
2023-07-24 16:29:58 -0400

    New build for use with new backend.

---
+++ src/components/SettingsPanel/SettingsPanel.jsx
@@ -0,0 +1,11 @@
+import '../SettingsPanel/SettingsPanel.scss'
+
+function SettingsPanel({display}){
+    return (
+        <div className={`panel ${display && 'panel--animation'}`}>
+
+        </div>
+    )
+}
+
+export default SettingsPanel
---
+++ src/pages/PuzzlePage.jsx
@@ -0,0 +1,41 @@
+import PuzzleBoard from "../components/PuzzleBoard/PuzzleBoard"
+import "../pages/PuzzlePage.scss"
+import axios from "axios";
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from "react-router"
+
+function PuzzlePage({ category, categoryRange }) {
+    const [movesObjectNotation, setMovesObjectNotation] = useState("")
+    const [positionFEN, setPositionFEN] = useState("")
+    const puzzleID = useParams().id
+    const navigate = useNavigate();
+    const apiURL = `https://tactics-db-api-gcp-wqrtz47qla-uc.a.run.app${category}/${puzzleID}`
+    console.log(apiURL)
+    useEffect(() => {
+        axios.get(apiURL).then(response => {
+            setMovesObjectNotation(response.data.Moves);
+            setPositionFEN(response.data.FEN);
+        }).catch(response => {
+            console.error(response);
+        })
+    }, [apiURL])
+    if (!positionFEN || !movesObjectNotation) {
+        return <>
+            Loading...
+        </>
+    }
+    return (
+        <>
+            <div className="board-container">
+                <PuzzleBoard positionFEN={positionFEN} movesArray={movesObjectNotation.split(' ')} orientation={positionFEN.indexOf('b') > positionFEN.indexOf('w') ? "white" : "black"} />
+            </div>
+            <div className="navpanel">
+                <button className="navbutton navbutton--backward"></button>
+                <button className="navbutton navbutton--forward" onClick={() => { navigate(`${category}/${Math.ceil(Math.random() * categoryRange)}`) }}></button>
+            </div>
+        </>
+    )
+}
+
+
+export default PuzzlePage
