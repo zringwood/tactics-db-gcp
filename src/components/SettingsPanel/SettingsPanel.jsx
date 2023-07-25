52bde5403fc78c6bb7f59b37caa87e93f3efb9c6
Zachary Ringwood <zringwood@gmail.com>
2023-07-25 15:51:13 -0400

    difficulty slider implemented

--- src/components/SettingsPanel/SettingsPanel.jsx
+++ src/components/SettingsPanel/SettingsPanel.jsx
@@ -3,26 +3,34 @@ import { Swipe } from "react-swipe-component"
 import {useState} from 'react'
 function SettingsPanel({ setIsShowingSettings, animate }) {
     const [position, setPosition] = useState(0)
+    const [difficulty, setDifficulty] = useState(1)
+    
     const onSwipeRightListener = () => {
         setIsShowingSettings(false)
     }
     const onSwipeListener = (p) => {
-        if (p.x !== 0) {
-          setPosition(p.x)
-        }
+        // if (p.x !== 0) {
+        //   setPosition(p.x)
+        // }
     }
     return (
-        <>
+        
     <Swipe
             nodeName="div"
             className="panel panel--animation"
             detectTouch={true}
-            delta = "65"
+            detectMouse={false}
+            delta = "60"
             onSwipedRight={onSwipeRightListener}
             onSwipe={onSwipeListener}
-            // style={{right: (16-position) + "px"}}
-            />
-            </>
+            style={{right: (16-position) + "px"}}
+            >
+            <form className='settingsform'>
+                <label htmlFor='settingsform__difficulty' className='settingsform__label'>Difficulty</label>
+                <input type="range" className='settingsform__difficulty' id='settingsform__difficulty' name="difficulty" min="700" max="2500" value={difficulty} onChange={(evt) => setDifficulty(evt.target.value)}/>
+                <button type="submit" className='settingsform__submit'>New Puzzle</button>
+            </form>
+            </Swipe>
     )
 }
 
