1af5152c34101797e1aaf152de09a31561b78efb
Zachary Ringwood <zringwood@gmail.com>
2023-07-26 13:37:01 -0400

    Added Click Controls.

--- src/components/PuzzleBoard/PuzzleBoard.jsx
+++ src/components/PuzzleBoard/PuzzleBoard.jsx
@@ -7,13 +7,46 @@ function PuzzleBoard({ positionFEN, movesArray, orientation }) {
     const [moveLogic, setMoveLogic] = useState(new Chess(positionFEN))
     //Logic to perform an animation when the wrong move is played
     const [isWrongMove, setIsWrongMove] = useState(false)
+    //Used for click functionality
+    const [selectedSquare, setSelectedSquare] = useState(undefined)
+    //Used for highlighting squares
+    const [highlightSquares, setHighlightSquares] = useState({})
     const moveIndex = useRef(0)
+    console.log(movesArray)
     //This is needed to trigger a rerender in the chessboard component. 
     useEffect(() => {
         setMoveLogic(new Chess(positionFEN));
         //moveIndex must be reset when the puzzle resets.
         moveIndex.current = 0;
     }, [positionFEN])
+
+    const onSquareClick = (square) => {
+        console.log(moveIndex.current, `${selectedSquare}${square}`)
+        if (!selectedSquare) {
+            setSelectedSquare(square)
+            const newSquares = {}
+            newSquares[square] = { background: "rgba(255, 0, 0, 0.4)" }
+            setHighlightSquares(newSquares)
+        } else if (movesArray[moveIndex.current] === `${selectedSquare}${square}`) {
+            updatePuzzle(`${selectedSquare}${square}`)
+            if (!isEndofPuzzle()) {
+                const halfSecond = 500
+                setTimeout(() => {
+                    updatePuzzle(movesArray[moveIndex.current])
+                }, halfSecond)
+            }
+            setSelectedSquare(undefined)
+            setHighlightSquares({})
+        }
+        else {
+            setSelectedSquare(undefined)
+            setHighlightSquares({})
+        }
+
+
+    }
+
+
     const onDrop = (sourceSquare, targetSquare) => {
         let move = `${sourceSquare}${targetSquare}`
         if (isCorrectMove(move)) {
@@ -52,10 +85,21 @@ function PuzzleBoard({ positionFEN, movesArray, orientation }) {
     }
     return (
         <>
-            <Chessboard position={moveLogic.fen()} onPieceDrop={onDrop} boardOrientation={orientation} customBoardStyle={{boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5 ',
-    borderRadius: "24px"}}/>
+            <Chessboard position={moveLogic.fen()}
+                onPieceDrop={onDrop}
+                onSquareClick={onSquareClick}
+                boardOrientation={orientation}
+                customBoardStyle={
+                    {
+                        boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5 ',
+                        borderRadius: "24px"
+                    }
+                }
+                customSquareStyles={{
+                    ...highlightSquares
+                }} />
             {moveIndex.current >= movesArray.length && <p>You Win!</p>}
-           {isWrongMove && <p> Try Again!</p>}
+            {isWrongMove && <p> Try Again!</p>}
         </>
     )
 }
