279970bf27742f010aae966f90385aa6dc5b9994
Zachary Ringwood <zringwood@gmail.com>
2023-07-27 16:49:06 -0400

    Styling added to settings panel.

--- src/components/SettingsPanel/SettingsPanel.jsx
+++ src/components/SettingsPanel/SettingsPanel.jsx
@@ -40,17 +40,24 @@ function SettingsPanel({ setIsShowingSettings, ranges }) {
             >
                 <button className='panel__exit' onClick={() => setIsShowingSettings(false)}/>
             <form className='settingsform' onSubmit={onSubmit}>
-                <p className='settingsform__label'>{checkedRadio}</p>
+            <div className='flex-container'>
+                <p className='settingsform__label settingsform__label--radio'>{checkedRadio}</p>
                 <div className='radiobuttons'>
                 <input type="radio" className='settingsform__difficulty--easy' id='Easy' name="difficulty" value="easy" defaultChecked onChange={(evt) => setCheckedRadio(evt.target.id)}/>
                 <input type="radio" className='settingsform__difficulty--medium' id='Medium' name="difficulty" value="medium" onChange={(evt) => setCheckedRadio(evt.target.id)}/>
                 <input type="radio" className='settingsform__difficulty--hard' id='Hard' name="difficulty" value="hard" onChange={(evt) => setCheckedRadio(evt.target.id)}/>
-                <input type="radio" className='settingsform__difficulty--grandmaster' id='Grandmaster' name="difficulty" value="grandmaster" onChange={(evt) => setCheckedRadio(evt.target.id)} />
+                <input type="radio" className='settingsform__difficulty--grandmaster' id='GM' name="difficulty" value="grandmaster" onChange={(evt) => setCheckedRadio(evt.target.id)} />
                 <input type="radio" className='settingsform__difficulty--engine' id='Engine' name="difficulty" value="engine" onChange={(evt) => setCheckedRadio(evt.target.id)}/>
                 </div>
-                <label htmlFor='hideTitles'>Hide Titles?</label>
+                </div>
+                <div className='flex-container'>
+                <label htmlFor='hideTitles'>Hide Titles</label>
                 <input type="checkbox" className='settingsform__titles' id="hideTitles" name="hideTitle" />
-                
+                </div>
+                <div className='flex-container'>
+                <label htmlFor='autoServe'>Serve Puzzles Automatically</label>
+                <input type="checkbox" className='settingsform__autoserve' id="autoserve" name="autoserve" />
+                </div>
                 <button type="submit" className='settingsform__submit'>New Puzzle</button>
                
             </form>
