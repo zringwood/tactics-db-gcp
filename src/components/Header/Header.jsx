389d634a47519175b18b0c6fce9c6de28781a009
Zachary Ringwood <zringwood@gmail.com>
2023-07-27 16:30:43 -0400

    Removed the navlinks.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,6 @@ function Header({ranges}) {
     const [isShowingSettings, setIsShowingSettings] = useState(false)
     return ( 
         <header className='navbar'>
-            <NavLink className="navbar__link" to={`/`}><p className='navbar__text'>Middlegames</p></NavLink>
-            <NavLink className="navbar__link" to={`/`}><p className='navbar__text'>Endgames</p></NavLink>
             {isShowingSettings && <SettingsPanel setIsShowingSettings={setIsShowingSettings} ranges={ranges}/>}
             <button className={`navbar__settings ${isShowingSettings && "navbar__settings--animate"}`} onClick={() => setIsShowingSettings(!isShowingSettings)}></button>
         </header>
