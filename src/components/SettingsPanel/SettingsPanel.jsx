a31d9bc686830f447df8959a5c38514a421cc0e6
Zachary Ringwood <zringwood@gmail.com>
2023-07-31 16:30:37 -0400

    Form now saves values.

--- src/components/SettingsPanel/SettingsPanel.jsx
+++ src/components/SettingsPanel/SettingsPanel.jsx
@@ -4,10 +4,10 @@ import { useState} from 'react'
 import { useNavigate } from 'react-router'
 function SettingsPanel({ setIsShowingSettings, ranges }) {
     const [position, setPosition] = useState(0)
-    const [checkedRadio, setCheckedRadio] = useState(localStorage.getItem('difficulty') || "Easy")
-    const [checkedCategory, setCheckedCategory] = useState(localStorage.getItem('category') || "Middlegames")
-    const [autoserve, setAutoServe] = useState(localStorage.getItem('autoserve') || "off")
-    const [hideTitles, setHideTitles] = useState(localStorage.getItem('hideTitles') || "off")
+    const [checkedRadio, setCheckedRadio] = useState(localStorage.getItem('difficulty') || "easy")
+    const [checkedCategory, setCheckedCategory] = useState(localStorage.getItem('category') || "middlegames")
+    const [autoserve, setAutoServe] = useState(localStorage.getItem('autoserve'))
+    const [hideTitles, setHideTitles] = useState(localStorage.getItem('hideTitles'))
     const navigate = useNavigate()
     const onSwipeRightListener = () => {
         setIsShowingSettings(false)
@@ -19,14 +19,15 @@ function SettingsPanel({ setIsShowingSettings, ranges }) {
     }
     const onSubmit = (evt) => {
         evt.preventDefault()
-        const difficulty = evt.target.difficulty.value;
         setIsShowingSettings(false)
-        const category = evt.target.category.value
-        localStorage.setItem("difficulty", difficulty)
-        localStorage.setItem("category", category)
+        localStorage.setItem("difficulty", checkedRadio)
+        localStorage.setItem("category", checkedCategory)
         localStorage.setItem("hideTitles", hideTitles)
         localStorage.setItem("autoserve",autoserve)
-        navigate(`${category}/${difficulty}/${Math.ceil(Math.random() * ranges[`${category}_${difficulty}`])}`)
+
+        console.log("Auto Serve: ", autoserve)
+        console.log("Hide Titles: ", hideTitles)
+        navigate(`${checkedCategory}/${checkedRadio}/${Math.ceil(Math.random() * ranges[`${checkedCategory}_${checkedRadio}`])}`)
     }
 
     return (
@@ -47,33 +48,33 @@ function SettingsPanel({ setIsShowingSettings, ranges }) {
                 
                 <label htmlFor='difficulty'>Difficulty</label>
                 <div className='flex-container'>
-                    <p className='settingsform__label settingsform__label--radio'>{checkedRadio}</p>
+                    <p className='settingsform__label settingsform__label--radio'>{capitalize(checkedRadio)}</p>
                     <div className='radiobuttons'>
-                        <input type="radio" className='settingsform__radio' id='Easy' name="difficulty" value="easy" defaultChecked onChange={(evt) => setCheckedRadio(evt.target.id)} />
-                        <input type="radio" className='settingsform__radio' id='Medium' name="difficulty" value="medium" onChange={(evt) => setCheckedRadio(evt.target.id)} />
-                        <input type="radio" className='settingsform__radio' id='Hard' name="difficulty" value="hard" onChange={(evt) => setCheckedRadio(evt.target.id)} />
-                        <input type="radio" className='settingsform__radio' id='GM' name="difficulty" value="grandmaster" onChange={(evt) => setCheckedRadio(evt.target.id)} />
-                        <input type="radio" className='settingsform__radio' id='Engine' name="difficulty" value="engine" onChange={(evt) => setCheckedRadio(evt.target.id)} />
+                        <input type="radio" className='settingsform__radio' id='Easy' name="difficulty" value="easy" defaultChecked={localStorage.getItem('difficulty')==='easy'} onChange={(evt) => setCheckedRadio(evt.target.value)} />
+                        <input type="radio" className='settingsform__radio' id='Medium' name="difficulty" value="medium"defaultChecked={localStorage.getItem('difficulty')==='medium'} onChange={(evt) => setCheckedRadio(evt.target.value)} />
+                        <input type="radio" className='settingsform__radio' id='Hard' name="difficulty" value="hard" defaultChecked={localStorage.getItem('difficulty')==='hard'}onChange={(evt) => setCheckedRadio(evt.target.value)} />
+                        <input type="radio" className='settingsform__radio' id='GM' name="difficulty" value="grandmaster" defaultChecked={localStorage.getItem('difficulty')==='grandmaster'}onChange={(evt) => setCheckedRadio(evt.target.value)} />
+                        <input type="radio" className='settingsform__radio' id='Engine' name="difficulty" value="engine"defaultChecked={localStorage.getItem('difficulty')==='engine'} onChange={(evt) => setCheckedRadio(evt.target.value)} />
                     </div>
                 </div>
                 
                 
                 <label htmlFor='category'>Category</label>
                 <div className='flex-container'>
-                    <p className='settingsform__label settingsform__label--radio'>{checkedCategory}</p>
+                    <p className='settingsform__label settingsform__label--radio'>{capitalize(checkedCategory)}</p>
                     <div className='radiobuttons'>
-                        <input type="radio" className='settingsform__radio' id='Middlegames' name="category" value="middlegames" defaultChecked onChange={(evt) => setCheckedCategory(evt.target.id)} />
-                        <input type="radio" className='settingsform__radio' id='Endgames' name="category" value="endgames" onChange={(evt) => setCheckedCategory(evt.target.id)} />
+                        <input type="radio" className='settingsform__radio' id='Middlegames' name="category" value="middlegames" defaultChecked={localStorage.getItem('category')==='middlegames'} onChange={(evt) => setCheckedCategory(evt.target.value)} />
+                        <input type="radio" className='settingsform__radio' id='Endgames' name="category" value="endgames" defaultChecked={localStorage.getItem('category')==='endgames'} onChange={(evt) => setCheckedCategory(evt.target.value)} />
                     </div>
                 </div>
                 
                 <div className='flex-container'>
                     <label htmlFor='hideTitles'>Hide Titles</label>
-                    <input type="checkbox" className='settingsform__checkbox' id="hideTitles" name="hideTitle" onChange={(evt) => setHideTitles(evt.target.value)}/>
+                    <input type="checkbox" className='settingsform__checkbox' id="hideTitles" name="hideTitle" defaultChecked={localStorage.getItem('hideTitles')==='true'} onChange={(evt) => {setHideTitles(evt.target.checked)}}/>
                 </div>
                 <div className='flex-container'>
                     <label htmlFor='autoServe'>Serve Puzzles Automatically</label>
-                    <input type="checkbox" className='settingsform__checkbox' id="autoserve" name="autoserve" onChange={(evt) => setAutoServe(evt.target.value)}/>
+                    <input type="checkbox" className='settingsform__checkbox' id="autoserve" name="autoserve"defaultChecked={localStorage.getItem('autoserve')==='true'} onChange={(evt) => setAutoServe(evt.target.checked)}/>
                 </div>
                
                 <button type="submit" className='settingsform__submit'>New Puzzle</button>
@@ -83,7 +84,14 @@ function SettingsPanel({ setIsShowingSettings, ranges }) {
         </Swipe>
     )
 }
-
+function capitalize(str){
+    if(!!str){
+        if(str === 'grandmaster')
+            return 'GM'
+        return `${str[0].toUpperCase()}${str.slice(1)}`
+    }
+    return null
+}
 
 
 
